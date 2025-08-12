5ff84abded884e0aae55fc80212971c9f06b256f
Evgeniy19021983 <evgeniyd1983@ukr.net>
2025-08-12 10:23:17 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src',
-  base: process.env.NODE_ENV === 'production' ? '/goit-js-hw-12/' : '/',
+  base: '/goit-js-hw-12/',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
